da82e162bea76299ecef98ad6307adfa9b5664e3
augustlewen <augustlewen@live.com>
2023-10-04 15:32:18 +0200

    added trap

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Trap : MonoBehaviour
+{ 
+    IEnumerator OnCollisionEnter(Collision other)
+    {
+        Destroy(other.gameObject);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(0);
+
+    }
+}
