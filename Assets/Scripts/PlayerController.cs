b909369abbe73d2dac6ae09d965b9f2b2a8b7c27
augustlewen <augustlewen@live.com>
2023-10-05 10:24:02 +0200

    added jump to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+   private Rigidbody rb;
+   public int moveSpeed;
+   public int jumpForce;
+   public KeyCode jumpKey;
+   private void Start()
+   {
+      rb = GetComponent<Rigidbody>();
+   }
+
    private void Update()
    {
-      transform.Translate(0, 0, 5 * Time.deltaTime);
+      transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+      bool isKeyPressed = Input.GetKeyDown(jumpKey);
+
+      if (isKeyPressed)
+      {
+         rb.AddForce(0, jumpForce, 0);
+         Debug.Log(rb.velocity);
+      }
    }
+    
 }
