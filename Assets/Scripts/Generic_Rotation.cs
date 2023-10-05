fbfa4da95774ed29c9e0287d8bd7467ddb73698a
augustlewen <augustlewen@live.com>
2023-10-05 10:24:16 +0200

    Updated camera rotation script and camera position

--- Assets/Scripts/Generic_Rotation.cs
+++ Assets/Scripts/Generic_Rotation.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Generic_Rotation : MonoBehaviour
 {
-    
-    public int rotationSpeed;
+    public int maxRotationSpeed;
+    float rotationSpeed = 0;
+    public float maxAngle;
 
     void Update()
     {
+        rotationSpeed = Mathf.Lerp(rotationSpeed, maxRotationSpeed, 0.1f);
         transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        
+        if(transform.rotation.z < maxAngle && transform.rotation.z > -maxAngle)
+            return;
+
+        maxRotationSpeed = -maxRotationSpeed;
+        rotationSpeed = 0;
     }
 }
