bfbb6e2fccb123015fc7cdcc8b31e4f8024bda14
augustlewen <augustlewen@live.com>
2023-10-09 11:30:08 +0200

    Fixed exception on player death bug

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -8,6 +8,8 @@ namespace DefaultNamespace
         private void LateUpdate()
         {
             GameObject player = GameObject.Find("Player");
+            if (player == null)
+                return;
             Vector3 targetPosition = player.transform.position + new Vector3(0, 2.64f, -3.33f);
             transform.position = targetPosition;
         }
