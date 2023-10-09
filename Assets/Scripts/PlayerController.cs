5eec01e50ed1bc2218410c612b57151071cd24e4
augustlewen <augustlewen@live.com>
2023-10-09 11:25:13 +0200

    made the player move with physics instead of translate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,22 +9,33 @@ public class PlayerController : MonoBehaviour
    public int moveSpeed;
    public int jumpForce;
    public KeyCode jumpKey;
+   public int spinSpeed;
 
-   
+   private void FixedUpdate()
+   {
+      Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+      if(rigidbody.velocity.y < -0.1f)
+         rigidbody.AddForce(0, -1, 0);
+         
+      rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, moveSpeed);
+   }
 
    private void Update()
    {
-      GetComponent<Rigidbody>().angularVelocity = new Vector3(8f, 0, 0);
-
-      transform.Translate(0, 0, moveSpeed * Time.deltaTime, relativeTo:Space.World);
+      Rigidbody rigidbody = GetComponent<Rigidbody>();
+      rigidbody.angularVelocity = new Vector3(spinSpeed, 0, 0);
 
       if (Input.GetKeyDown(jumpKey) && IsTouchingGround())
-      {
-         Rigidbody rigidbody = GetComponent<Rigidbody>();
-         rigidbody.AddForce(0, jumpForce, 0);
-         rigidbody.angularVelocity = new Vector3(15f, 0, 0);
+         Jump();
+      
+   }
 
-      }
+   private void Jump()
+   {
+      Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+      rigidbody.AddForce(0, jumpForce, 0);
    }
 
    bool IsTouchingGround()
